19acec015b593077d6eb667c268d388e750c7842
MapkoX <ayman.nadjoui.2001@gmail.com>
2024-10-31 19:14:01 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,19 @@
+var array_news=[];
+var array_anime_rating=[];
+var array_character_rating=[];
+
+
+// news
+array_news.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
+array_news.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko2</a>');
+array_news.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko3</a>');
+
+// anime rating
+array_anime_rating.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
+array_anime_rating.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
+array_anime_rating.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
+
+// character rating
+array_character_rating.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
+array_character_rating.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
+array_character_rating.push('<a href="gff"><img class="img1" src="images/a.jpg">mapko1</a>');
--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+// splash
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("splash-screen").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }, 4000);
+});
+ 
+ // refreshing
+setInterval(function refresh() {
+  refresh_news();
+  anime_rating();
+  character_rating();
+},1000);
 
+function refresh_news() {
+  var news=document.getElementById('news');
+  news.innerHTML='<h3>news</h3><ul><li>'+array_news[0]+'</li><li>'+array_news[1]+'</li><li>'+array_news[2]+'</li></ul>';
+}
+function anime_rating() {
+  var anime_rating=document.getElementById('anime_rating');
+  anime_rating.innerHTML='<h3>anime rating</h3><ul><li>'+array_anime_rating[0]+'</li><li>'+array_anime_rating[1]+'</li><li>'+array_anime_rating[2]+'</li></ul>';
+}
+function character_rating() {
+  var character_rating=document.getElementById('character_rating');
+  character_rating.innerHTML='<h3>character rating</h3><ul><li>'+array_character_rating[0]+'</li><li>'+array_character_rating[1]+'</li><li>'+array_character_rating[2]+'</li></ul>';
+}
